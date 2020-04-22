115c0b40093c3e08c113313bf0fc690d6a17c4cb
Christian Zufferey <christian@zufferey.com>
2020-04-22 21:10:54 +0200

    Corrigé le problème du nouveau non zuzu-test.ml

--- msl_mysql.sh
+++ msl_mysql.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #petit script pour lancer un ssh forward pour le serveur mysql du MsL
-#zf180128.0048, zf191025.1423
+#zf180128.0048, zf191025.1423, zf200422.2106
 
 # A lancer directement depuis une machine linux au MsL
 
-ssh ubuntu@www.zuzutest.ml ssh -N -g -L 20881:192.168.1.99:8081 ubuntu@localhost -p 20221 &
+ssh ubuntu@www.zuzu-test.ml ssh -N -g -L 20881:192.168.1.99:8081 ubuntu@localhost -p 20221 &
 
-sleep 2
+#sleep 2
 
-ssh ubuntu@www.zuzutest.ml netstat -nat
+#ssh ubuntu@www.zuzu-test.ml netstat -nat
 
